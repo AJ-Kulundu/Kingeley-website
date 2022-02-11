c93473cab1619f9666cca968fcf61297159506f6
AJ-Kulundu <jameskulundu@gmail.com>
2022-02-11 14:00:40 +0300

    Adding a footer component and header details for the website

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head';
+
+
+const Header = () =>  {
+  return (
+    <Head>
+        <title>Kingley Agency</title>
+        <meta name="description" content="Kingeley Agencies website" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/Kingeley logo.png" />
+      </Head>
+  )
+}
+
+export default Header
